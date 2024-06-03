dd33e185bfdf0d631a73b2c54bc60a10b5859355
Jyebin <yebin0322@naver.com>
2024-06-03 17:44:14 +0900

    Feat : PaymentDTO 작성 - 아임포트 결제 관련 정보만 포함된 DTO

---
+++ src/main/java/com/choikang/chukahaeyo/payment/PaymentDTO.java
@@ -0,0 +1,16 @@
+package com.choikang.chukahaeyo.payment;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class PaymentDTO {
+    private String applyNum; //카드 승인 번호
+    private String buyer_email; //구매자 이메일 주소
+    private String impUid; // 아임포트 결제 고유 번호
+    private String merchantUid; //가맹점 주문 번호. 각 주문 건에 대해 유일한 식별자를 가짐
+    private int paidAmount; //실제로 결제 된 금액
+    private Date paidAt; // 결제 완료 시간
+    private String status; // 결제 여부
+    private String receiptUrl; //영수증 url
+}
