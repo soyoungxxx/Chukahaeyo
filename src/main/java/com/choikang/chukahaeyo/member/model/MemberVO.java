75e0cb349f11bed5b761308d83c46784dafcda0c
cmsxi <cms0755@naver.com>
2024-06-03 10:21:41 +0900

    init: MemberVO

--- src/main/java/com/choikang/chukahaeyo/member/model/MemberVO.java
+++ src/main/java/com/choikang/chukahaeyo/member/model/MemberVO.java
@@ -1,4 +1,15 @@
-package member.model;
+package com.choikang.chukahaeyo.member.model;
 
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
 public class MemberVO {
+    private int userId;
+    private String userEmail;
+    private String userPwd;
+    private String userName;
+    private Date registerDate;
+    private boolean auth;
 }
