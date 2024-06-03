7bdb6d1406ea0884a5d9cf8680efd01fc87d0116
Jyebin <yebin0322@naver.com>
2024-06-03 17:43:39 +0900

    Feat : PaymentController 작성 - edit 페이지로 접속하는 api 작성 - 결제 완료 후 응답값을 받아와 처리하는 로직 작성중

--- src/main/java/com/choikang/chukahaeyo/payment/PaymentController.java
+++ src/main/java/com/choikang/chukahaeyo/payment/PaymentController.java
@@ -1,4 +1,42 @@
-package payment;
+package com.choikang.chukahaeyo.payment;
 
+
+import com.choikang.chukahaeyo.exception.ErrorCode;
+import com.choikang.chukahaeyo.exception.SuccessCode;
+import com.choikang.chukahaeyo.payment.model.PaymentVO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+@RequestMapping("/payments")
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/edit")
+    public String showEditPage() {
+        return "card/edit";
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processPayment(@RequestBody PaymentDTO paymentDTO) {
+        try{
+            paymentService.processPayment(paymentDTO);
+            return new ResponseEntity<>(SuccessCode.PAYMENT_SUCCESS.getMessage(),SuccessCode.PAYMENT_SUCCESS.getHttpStatus());
+        }catch (Exception e){
+            return new ResponseEntity<>(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
+        }
+    }
 }
