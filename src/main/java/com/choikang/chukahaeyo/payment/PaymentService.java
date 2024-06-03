9a084ea826d5135c21ee8601a7949c722ef18797
Jyebin <yebin0322@naver.com>
2024-06-03 17:46:13 +0900

    Feat : PaymentService 작성 - 받아온 정보를 Mapper로 넘겨주는 로직 작성 - 추후 수정 및 추가 예정

--- src/main/java/com/choikang/chukahaeyo/payment/PaymentService.java
+++ src/main/java/com/choikang/chukahaeyo/payment/PaymentService.java
@@ -1,4 +1,30 @@
-package payment;
+package com.choikang.chukahaeyo.payment;
 
+import com.choikang.chukahaeyo.payment.model.PaymentVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
 public class PaymentService {
+    @Autowired
+    private PaymentMapper paymentMapper;
+
+    @Transactional
+    public void processPayment(PaymentDTO paymentDTO) {
+        PaymentVO paymentVO = new PaymentVO();
+        paymentVO.setApplyNum(paymentDTO.getApplyNum());
+        paymentVO.setBuyer_email(paymentDTO.getBuyer_email());
+        paymentVO.setImpUid(paymentDTO.getImpUid());
+        paymentVO.setMerchantUid(paymentDTO.getMerchantUid());
+        paymentVO.setPaidAmount(paymentDTO.getPaidAmount());
+        paymentVO.setPaidAt(paymentDTO.getPaidAt());
+        paymentVO.setStatus(paymentDTO.getStatus());
+        paymentVO.setReceiptUrl(paymentDTO.getReceiptUrl());
+
+        paymentMapper.insertPayment(paymentVO);
+    }
+
 }
