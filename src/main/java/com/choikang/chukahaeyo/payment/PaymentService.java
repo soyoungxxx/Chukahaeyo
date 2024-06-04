73610049cdae11621006f75bdcb974ce3020da63
Jyebin <yebin0322@naver.com>
2024-06-04 11:35:55 +0900

    Feat : PaymentService 구현 완료 - 추후 transactional 추가 예정 - 기능 구현 및 테스트 완료 후 테스트 용을 위한 print문 삭제 예정

--- src/main/java/com/choikang/chukahaeyo/payment/PaymentService.java
+++ src/main/java/com/choikang/chukahaeyo/payment/PaymentService.java
@@ -1,30 +1,33 @@
 package com.choikang.chukahaeyo.payment;
 
+
 import com.choikang.chukahaeyo.payment.model.PaymentVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 
 @Service
 public class PaymentService {
     @Autowired
     private PaymentMapper paymentMapper;
 
-    @Transactional
+    //@Transactional
     public void processPayment(PaymentDTO paymentDTO) {
-        PaymentVO paymentVO = new PaymentVO();
-        paymentVO.setApplyNum(paymentDTO.getApplyNum());
-        paymentVO.setBuyer_email(paymentDTO.getBuyer_email());
-        paymentVO.setImpUid(paymentDTO.getImpUid());
-        paymentVO.setMerchantUid(paymentDTO.getMerchantUid());
-        paymentVO.setPaidAmount(paymentDTO.getPaidAmount());
-        paymentVO.setPaidAt(paymentDTO.getPaidAt());
-        paymentVO.setStatus(paymentDTO.getStatus());
-        paymentVO.setReceiptUrl(paymentDTO.getReceiptUrl());
-
-        paymentMapper.insertPayment(paymentVO);
+        System.out.println("서비스dto"+paymentDTO);
+        try{
+            System.out.println("서비스1");
+            PaymentVO paymentVO = PaymentDTO.of(paymentDTO);
+            System.out.println("서비스2");
+            paymentMapper.insertPayment(paymentVO);
+            System.out.println("서비스3");
+            System.out.println("[service] DTO를 VO로 변환 완료");
+        }catch(Exception e){
+            System.out.println("서비스4");
+            System.out.println("[service] DTO를 VO로 변환 실패");
+            System.out.println("서비스5");
+            throw e;
+        }
     }
 
 }
