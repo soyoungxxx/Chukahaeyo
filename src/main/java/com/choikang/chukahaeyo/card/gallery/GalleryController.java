2a3a2c6dba619e997c4add2f585bc945362c26d8
wing-beat <wingbeat1115@gmail.com>
2024-06-04 12:40:34 +0900

    feat: Add GalleryPage.jsp - 명예의 전당 페이지 프레임 완성 - CardTest 임의 파일은 나중에 삭제 필요

--- src/main/java/com/choikang/chukahaeyo/card/gallery/GalleryController.java
+++ src/main/java/com/choikang/chukahaeyo/card/gallery/GalleryController.java
@@ -1,12 +1,22 @@
 package com.choikang.chukahaeyo.card.gallery;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class GalleryController {
     @GetMapping("/gallery")
-    public String gallery() {
+    public String gallery(Model model) {
+        List<CardTest> cards = new ArrayList<>();
+        cards.add(new CardTest("💌 Card 1", "2023-06-01", 10, "", "template01"));
+        cards.add(new CardTest("💌 Card 2", "2023-05-15", 20, "", "template02"));
+        cards.add(new CardTest("💌 Card 3", "2023-04-30", 5, "", "template03"));
+
+        model.addAttribute("cards", cards);
         return "card/gallery";
     }
 }
