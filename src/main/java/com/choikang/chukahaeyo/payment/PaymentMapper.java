ce6dd148bae5800a9d12a52cde93ef4c9720f7c0
Jyebin <yebin0322@naver.com>
2024-06-04 11:33:27 +0900

    Feat : PaymentMapper 작성 - 결제 정보를 DB에 저장하는 insertPayment 작성 - 추후 기능 추가 예정

--- src/main/java/com/choikang/chukahaeyo/payment/PaymentMapper.java
+++ src/main/java/com/choikang/chukahaeyo/payment/PaymentMapper.java
@@ -2,11 +2,9 @@ package com.choikang.chukahaeyo.payment;
 
 import com.choikang.chukahaeyo.payment.model.PaymentVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Insert;
+
 
 @Mapper
 public interface PaymentMapper {
     void insertPayment(PaymentVO paymentVO); //결제 정보를 DB에 저장
-
 }
