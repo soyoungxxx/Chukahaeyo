a8e80a700f3f0fa2fa9c381481101be54df7bb50
Jyebin <yebin0322@naver.com>
2024-06-08 17:14:52 +0900

    Comment : url에 주석 추가

--- src/main/java/com/choikang/chukahaeyo/card/model/CardVO.java
+++ src/main/java/com/choikang/chukahaeyo/card/model/CardVO.java
@@ -33,6 +33,7 @@ public class CardVO {
     private String account;
     private int pay_id;
     private String url; //단축 url
+    private String qr; //qr코드. 단축 URL의 끝에 .qr을 붙이면 그것이 이미지 주소
     private boolean isPayed;
     private int user_id;
 }
