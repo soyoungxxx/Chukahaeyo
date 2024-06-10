c058a1406e639822dce1617799b346bdde5d9feb
soyoungxxx <otherplet@gmail.com>
2024-06-10 19:46:51 +0900

    feat: Add kakao address api code

--- src/main/java/com/choikang/chukahaeyo/card/edit/EditController.java
+++ src/main/java/com/choikang/chukahaeyo/card/edit/EditController.java
@@ -6,17 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/card", produces = "application/text; charset=utf8")
+@RequestMapping( "/card")
 public class EditController {
     @Autowired
     private EditService service;
@@ -32,19 +36,14 @@ public class EditController {
 
     @ResponseBody
     @GetMapping(value="/edit/template.do", produces="text/html; charset=UTF-8")
-    public String getPreviewTemplate(int id, HttpServletResponse response) {
-//        System.out.println(service.selectPreviewFrame(id));
-//        response.setCharacterEncoding("UTF-8");
+    public String getPreviewTemplate(int id) {
         return service.selectPreviewFrame(id);
     }
 
 
     @PostMapping("/edit/card.do")
     public String getCardInfo(CardVO cardVO) {
-        // 세션에 있는 값 가져와서 user_id setting 하기 - 아직 미구현
-        // url -> 일단 null값으로 두기
-        // pay_id => null
-        System.out.println("!!!!!!!!!!!1");
+
         return "redirect:/cart";
     }
 
@@ -53,11 +52,29 @@ public class EditController {
     @Value("${API_URL}")
     private String API_URL;
 
-    @GetMapping(value="/map", produces = "application/text; charset=utf8")
+    @GetMapping("/map")
     public String getKakaoApiFromAddress(@RequestParam("query") String roadFullAddr) {
         try {
-            roadFullAddr = URLEncoder.encode(roadFullAddr, StandardCharsets.UTF_8);
-            System.out.println(roadFullAddr);
+            // 디코딩
+            roadFullAddr = URLDecoder.decode(roadFullAddr, StandardCharsets.UTF_8);
+            // url 생성
+            String addr = API_URL + "?query=" + roadFullAddr;
+            URL url = new URL(addr);
+            // url 커넥션 객체 생성
+            URLConnection conn = url.openConnection();
+            // 헤더값 설정
+            conn.setRequestProperty("Authorization", "KakaoAK" + REST_API_KEY);
+            // string buffer에 값 넣고 string 형태로 변환, jsonString return
+            BufferedReader br = null;
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+            StringBuffer docJson = new StringBuffer();
+            String line;
+            while ((line=br.readLine()) != null) {
+                docJson.append(line);
+            }
+
+
         } catch(Exception e) {
             e.printStackTrace();
         }
