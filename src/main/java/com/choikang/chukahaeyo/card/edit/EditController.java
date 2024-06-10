11c538a732efc61a61239c76e9c19abde1adffc5
soyoungxxx <otherplet@gmail.com>
2024-06-10 20:09:03 +0900

    feat: Update gitignore

--- src/main/java/com/choikang/chukahaeyo/card/edit/EditController.java
+++ src/main/java/com/choikang/chukahaeyo/card/edit/EditController.java
@@ -52,32 +52,32 @@ public class EditController {
     @Value("${API_URL}")
     private String API_URL;
 
-    @GetMapping("/map")
-    public String getKakaoApiFromAddress(@RequestParam("query") String roadFullAddr) {
-        try {
-            // 디코딩
-            roadFullAddr = URLDecoder.decode(roadFullAddr, StandardCharsets.UTF_8);
-            // url 생성
-            String addr = API_URL + "?query=" + roadFullAddr;
-            URL url = new URL(addr);
-            // url 커넥션 객체 생성
-            URLConnection conn = url.openConnection();
-            // 헤더값 설정
-            conn.setRequestProperty("Authorization", "KakaoAK" + REST_API_KEY);
-            // string buffer에 값 넣고 string 형태로 변환, jsonString return
-            BufferedReader br = null;
-            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-
-            StringBuffer docJson = new StringBuffer();
-            String line;
-            while ((line=br.readLine()) != null) {
-                docJson.append(line);
-            }
-
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return "redirect:/";
-    }
+//    @GetMapping("/map")
+//    public String getKakaoApiFromAddress(@RequestParam("query") String roadFullAddr) {
+//        try {
+//            // 디코딩
+//            roadFullAddr = URLDecoder.decode(roadFullAddr, StandardCharsets.UTF_8);
+//            // url 생성
+//            String addr = API_URL + "?query=" + roadFullAddr;
+//            URL url = new URL(addr);
+//            // url 커넥션 객체 생성
+//            URLConnection conn = url.openConnection();
+//            // 헤더값 설정
+//            conn.setRequestProperty("Authorization", "KakaoAK" + REST_API_KEY);
+//            // string buffer에 값 넣고 string 형태로 변환, jsonString return
+//            BufferedReader br = null;
+//            br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+//
+//            StringBuffer docJson = new StringBuffer();
+//            String line;
+//            while ((line=br.readLine()) != null) {
+//                docJson.append(line);
+//            }
+//
+//
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//        return "redirect:/";
+//    }
 }
