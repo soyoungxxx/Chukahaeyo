20c76f9050a521a2dc3ae99e0c6a6e329126362f
Jyebin <yebin0322@naver.com>
2024-06-10 17:10:58 +0900

    Feat : CardMapper 구현 - 추후 table명 변경 필요

--- src/main/java/com/choikang/chukahaeyo/card/CardMapper.java
+++ src/main/java/com/choikang/chukahaeyo/card/CardMapper.java
@@ -1,4 +1,9 @@
 package com.choikang.chukahaeyo.card;
 
+import com.choikang.chukahaeyo.card.model.CardVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface CardMapper {
+    void insertUrl(CardVO cardVO);
 }
