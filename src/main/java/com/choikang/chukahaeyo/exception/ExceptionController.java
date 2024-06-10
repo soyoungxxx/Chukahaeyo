d3e0a2d394ae3714855c613c5e91eef1262675dc
Jyebin <yebin0322@naver.com>
2024-06-10 09:51:33 +0900

    Feat : ExceptionController 구현

--- src/main/java/com/choikang/chukahaeyo/exception/ExceptionController.java
+++ src/main/java/com/choikang/chukahaeyo/exception/ExceptionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/errorPage", produces = "application/text; charset=utf8")
 public class ExceptionController {
+
     @GetMapping("404")
     public String show404ErrorPage(){
         return "error/404";
@@ -16,6 +17,4 @@ public class ExceptionController {
     public String show500ErrorPage(){
         return "error/500";
     }
-
-
 }
