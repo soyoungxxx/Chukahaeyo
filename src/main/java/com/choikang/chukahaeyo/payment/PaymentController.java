6946153fae47c3bedf08ac02daa3b64f44085025
Jyebin <yebin0322@naver.com>
2024-06-13 15:33:54 +0900

    Feat : PaymentController 작성 - 요청 보내는 부분 service로 이동 - /getToken 메소드 주석 처리 - 결제 취소 api 구현 및 테스트 완료

--- src/main/java/com/choikang/chukahaeyo/payment/PaymentController.java
+++ src/main/java/com/choikang/chukahaeyo/payment/PaymentController.java
@@ -3,77 +3,54 @@ package com.choikang.chukahaeyo.payment;
 
 import com.choikang.chukahaeyo.exception.ErrorCode;
 import com.choikang.chukahaeyo.exception.SuccessCode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Controller
 @RequestMapping(value = "/payments", produces = "application/text; charset=utf8")
-public class  PaymentController {
+public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
     @PostMapping("/process")
     public ResponseEntity<String> processPayment(@RequestBody PaymentDTO paymentDTO) {
         System.out.println(paymentDTO);
-        try{
+        try {
             paymentService.processPayment(paymentDTO);
-            return new ResponseEntity<>(SuccessCode.PAYMENT_SUCCESS.getMessage(),SuccessCode.PAYMENT_SUCCESS.getHttpStatus());
-        }catch (Exception e){
+            return new ResponseEntity<>(SuccessCode.PAYMENT_SUCCESS.getMessage(), SuccessCode.PAYMENT_SUCCESS.getHttpStatus());
+        } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
         }
     }
+
     @GetMapping("/success")
-    public String showSuccess(){
+    public String showSuccess() {
         return "/card/successPay";
     }
 
-    @PostMapping("/cancle")
-    public void cancelPayment(@RequestParam("payID") int paymentId,@RequestBody PaymentDTO paymentDTO){
+//토큰 가져오는 controller 주석 처리. 만약 추후에 필요하면 사용 예정. 현재로서는 서비스단에서 token을 받아오기 때문에 문제 없음
+//    @PostMapping("/getToken")
+//    public ResponseEntity<String> getToken() {
+//        try {
+//            paymentService.getToken();
+//            return new ResponseEntity<>(SuccessCode.GET_ACCESS_TOKEN.getMessage(), SuccessCode.GET_ACCESS_TOKEN.getHttpStatus());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return new ResponseEntity<>(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
+//        }
+//    }
 
-    }
-
-    @Value("${imp.key}")
-    private String key;
-
-    @Value("${imp.secret}")
-    private String secretKey;
-
-    @PostMapping("/getToken")
-    public ResponseEntity<String> getToken(){
-
-        System.out.println("key : " + key);
-        System.out.println("secret Key : " + secretKey);
-
-        try{
-            String apiUrl = "https://api.iamport.kr/users/getToken"; //요청을 보낼 api 주소
-
-            RestTemplate restTemplate = new RestTemplate(); //body 설정
-
-            HttpHeaders headers = new HttpHeaders(); //header 설정
-            headers.setContentType(MediaType.APPLICATION_JSON); //콘텐츠 타입을 JSON으로 설정
-
-            Map<String, String> keyMap = new HashMap<>();
-            keyMap.put("imp_key",key);
-            keyMap.put("imp_secret", secretKey);
-            ObjectMapper objectMapper = new ObjectMapper();
-            String keyJson = objectMapper.writeValueAsString(keyMap);
-            System.out.println(keyJson);
-            HttpEntity<String> requestEntity = new HttpEntity<>(keyJson, headers); //HttpEntity 객체 생성
-
-            return restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
-        }catch (Exception e){
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("토큰 발급에 실패하였습니다.");
+    @PostMapping("/cancel")
+    public ResponseEntity<String> cancelPayment(@RequestParam String payNo) {
+        try {
+            paymentService.cancelPayment(payNo);
+            return new ResponseEntity<>(SuccessCode.CANCEL_SUCCESS.getMessage(), SuccessCode.CANCEL_SUCCESS.getHttpStatus());
+        } catch (Exception e) {
+            return new ResponseEntity<>(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(), ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
         }
     }
 }
