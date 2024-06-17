876a60d22d846c9adc7475a219a9b0f3d28e379c
Jyebin <yebin0322@naver.com>
2024-06-17 11:25:13 +0900

    Feat : 디코딩 test code 작성

---
+++ src/test/java/DecodeTest.java
@@ -0,0 +1,22 @@
+import com.choikang.chukahaeyo.common.Decode;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {config.MvcConfig.class})
+@WebAppConfiguration
+@Slf4j
+public class DecodeTest {
+
+    @Test
+    public void getDecode() {
+        String a = Decode.unicodeDecode("\\uc548\\ub155\\ud558\\uc138\\uc694");
+        System.out.println("decode:" + a);
+    }
+}
