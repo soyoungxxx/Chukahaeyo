12ab7869c3bfba4d08b86e88c0e96682e43a8e02
soyoungxxx <otherplet@gmail.com>
2024-06-20 17:54:36 +0900

    feat: Add ajax

--- src/main/java/com/choikang/chukahaeyo/admin/AdminController.java
+++ src/main/java/com/choikang/chukahaeyo/admin/AdminController.java
@@ -6,17 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@ResponseBody
 public class AdminController {
     @Autowired
     PaymentService paymentService;
 
     @GetMapping("/admin/payment.do")
-    @ResponseBody
     public List<Integer> selectPaymentSum(String date) {
         System.out.println("!!!!!!!!!!!!!!!");
         return paymentService.selectPaymentSum(date);
+    }
+
+    @GetMapping("/admin/month/payment.do")
+    public List<Integer> selectMonthlySum(String date) {
+        List<Integer> list = new ArrayList<>();
+        return list;
     }
 }
