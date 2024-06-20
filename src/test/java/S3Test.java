a3f422c47a1277f9f38c8a08952086892588eaf7
Jyebin <yebin0322@naver.com>
2024-06-20 15:23:55 +0900

    Test : S3 test code 추가

---
+++ src/test/java/S3Test.java
@@ -0,0 +1,24 @@
+import com.choikang.chukahaeyo.s3.S3Service;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {config.MvcConfig.class})
+@WebAppConfiguration
+@Slf4j
+public class S3Test {
+    @Autowired
+    S3Service s3Service;
+
+    @Test
+    public void cancelTest(){
+        s3Service.deleteFile("d26e29fe-fb0b-4807-bb1d-2ea664aa0094.png");
+    }
+}
